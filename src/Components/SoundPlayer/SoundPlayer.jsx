0597b470d4a2c2a9e22dc10cb1a873c0d51922d0
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2024-01-08 17:20:51 -0700

    add svg for sound player style
    still need to work styles

--- src/Components/SoundPlayer/SoundPlayer.jsx
+++ src/Components/SoundPlayer/SoundPlayer.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './SoundPlayer.module.css';
+import Svg from '../Svg/Svg';
 
 const SoundPlayer = ({ sources, ariaLabel, className }) => {
   return (
     <figure className={styles.figure}>
+      <Svg symbolId='record-player'className={`${styles.recordPlayer} `}/>
       <figcaption className={styles.sound}>
         {ariaLabel}
-      </figcaption>
+      
       {sources && sources.length > 0 && (
         <audio controls loop aria-label={ariaLabel} className={className}>
           {sources.map((source, index) => (
             <source key={index} src={source.src} type={source.type} />
           ))}
           Sorry! Your browser does not support the audio element.
-        </audio>
-      )}
+          </audio>
+        )}
+      </figcaption>
     </figure>
   );
 };
