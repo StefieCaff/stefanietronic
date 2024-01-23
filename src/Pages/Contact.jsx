107710ffe431f17d9f75ae58d5c415877e6d3870
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2024-01-23 12:11:23 -0700

    Update Contact.jsx
    edits

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -9,24 +9,25 @@ const Contact = () => {
         <div>
             <h2>Reach out via email:</h2>
             <a href={`mailto:${email}`}>Email Stefie</a>
-            <p>Alternatively, I am available on the following social media platforms:</p>
+            <p>Alternatively, connect with me on social media platforms by clicking the icon below:</p>
             <ul>
                 <li>
-                    
-                    <a href="https://github.com/StefieCaff" target="_blank" rel="noopener noreferrer"><Svg symbolId="github" /></a>
+                    <a href="https://github.com/StefieCaff" target="_blank" rel="noopener noreferrer">
+                        <Svg symbolId="github" />
+                    </a>
                 </li>
                 <li>
-                    
-                    <a href="https://www.linkedin.com/in/stefanie-caffarel-888209113" target="_blank" rel="noopener noreferrer"><Svg symbolId="linkedin" /></a>
+                    <a href="https://www.linkedin.com/in/stefanie-caffarel-888209113" target="_blank" rel="noopener noreferrer">
+                        <Svg symbolId="linkedin" />
+                    </a>
                 </li>
                 <li>
-                    
-                    <a href="https://www.instagram.com/stefaju/?igsh=MXhtY2U2czYyb3Y0Yg%3D%3D&utm_source=qr" target="_blank" rel="noopener noreferrer"><Svg symbolId="instagram" /></a>
-                     <a href={instagramProfileUrl} target="_blank" rel="noopener noreferrer">
-        Visit My Instagram Profile
-      </a>
-      <QRCode value={instagramProfileUrl} />
+                    <a href="https://www.instagram.com/stefaju/?igsh=MXhtY2U2czYyb3Y0Yg%3D%3D&utm_source=qr" target="_blank" rel="noopener noreferrer">
+                        <Svg symbolId="instagram" />
+                        <QRCode value={instagramProfileUrl} />
+                    </a>
                 </li>
+                <li></li>
             </ul>
         </div>
     )
