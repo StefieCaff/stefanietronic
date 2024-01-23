ad59db33dbf33fbaed2eeb8a7eada5e5c197afad
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2024-01-23 12:01:35 -0700

    contact page skeleton
    add insta qr code and email

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Svg from '../Components/Svg/Svg';
+import QRCode from 'react-qr-code';
 
 const Contact = () => {
+    const email = 'stefiecaff@gmail.com';
+     const instagramProfileUrl = 'https://www.instagram.com/stefaju/?igsh=MXhtY2U2czYyb3Y0Yg%3D%3D&utm_source=qr';
     return (
         <div>
-            <h2>Please, be in touch</h2>
-            <p>I am available on the following social media platforms:</p>
+            <h2>Reach out via email:</h2>
+            <a href={`mailto:${email}`}>Email Stefie</a>
+            <p>Alternatively, I am available on the following social media platforms:</p>
             <ul>
                 <li>
                     
@@ -17,7 +21,11 @@ const Contact = () => {
                 </li>
                 <li>
                     
-                     <a href="https://www.instagram.com/stefaju/?igsh=MXhtY2U2czYyb3Y0Yg%3D%3D&utm_source=qr" target="_blank" rel="noopener noreferrer"><Svg symbolId="instagram" /></a>
+                    <a href="https://www.instagram.com/stefaju/?igsh=MXhtY2U2czYyb3Y0Yg%3D%3D&utm_source=qr" target="_blank" rel="noopener noreferrer"><Svg symbolId="instagram" /></a>
+                     <a href={instagramProfileUrl} target="_blank" rel="noopener noreferrer">
+        Visit My Instagram Profile
+      </a>
+      <QRCode value={instagramProfileUrl} />
                 </li>
             </ul>
         </div>
