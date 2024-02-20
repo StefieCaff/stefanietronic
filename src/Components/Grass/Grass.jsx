5edbefeed32fbf0427f35c2b15616985b719f8a1
StefieCaff <108297341+StefieCaff@users.noreply.github.com>
2024-02-20 13:13:48 -0700

    Grass component rough draft
    need to work on "bottom" and possibly adding random green colors

--- src/Components/Grass/Grass.jsx
+++ src/Components/Grass/Grass.jsx
@@ -2,17 +2,40 @@
 import React from 'react';
 import styles from './Grass.module.css';
 
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 const Grass = () => {
   return (
     <div className={styles.grassContainer}>
       {/* Front Row */}
-      {Array.from({ length: 45 }, (_, index) => (
-        <div key={`front-${index}`} className={`${styles.blade} ${styles['blade' + (index + 1)]}`} />
-      ))}
+      <div className={`${styles.row} ${styles.frontRow}`}>
+        {Array.from({ length: 45 }, (_, index) => (
+          <div
+            key={`front-${index}`}
+            className={styles.blade}
+            style={{
+              height: getRandomInt(40, 35), // Random height between 30px and 70px
+              marginRight: getRandomInt(1, 2), // Random margin between 1px and 2px
+            }}
+          />
+        ))}
+      </div>
       {/* Back Row */}
-      {Array.from({ length: 45 }, (_, index) => (
-        <div key={`back-${index}`} className={`${styles.blade} ${styles['blade' + (index + 1) + 'b']}`} />
-      ))}
+      <div className={`${styles.row} ${styles.backRow}`}>
+        {Array.from({ length: 200 }, (_, index) => (
+          <div
+            key={`back-${index}`}
+            className={`${styles.blade} ${styles.backBlade}`} // Add back-blade class
+            style={{
+              height: getRandomInt(25, 35), // Random height between 25px and 35px
+              marginRight: getRandomInt(2, 3), // Random margin between 2px and 3px
+              animationDuration: '10s', // Adjust animation duration for back row
+            }}
+          />
+        ))}
+      </div>
     </div>
   );
 };
